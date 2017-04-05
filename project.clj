f44ffd02418c46d44f7b4b455a4a1032027c489e
Bobby <bfaber@gmail.com>
2017-04-05 19:23:59 -0400

    project: add reqs for fn-fx GUI

--- project.clj
+++ project.clj
@@ -1,9 +1,14 @@
 (defproject ladders-interview "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
+  :description "this app is a tool for skills-practice, steal code at your own risk!"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.9.0-alpha15"]
                  [org.clojure/test.check "0.9.0"]
                  [org.clojure/core.async "0.3.442"]
-                 [medley "0.8.4"]])
+                 [medley "0.8.4"]]
+  :main ladders-interview.javafx-init
+  :aot [ladders-interview.javafx-init]
+  :target-path "target/%s"
+  :profiles {:uberjar {:aot :all}})
+
